8203922f05757652fb7273713b30d0bb1e08da69
sieg <1357852347@qq.com>
2018-12-05 20:33:45 +0800

    Update 1.4.2.py

--- ch01/1.4.2.py
+++ ch01/1.4.2.py
@@ -1,4 +1,60 @@
-#coding:utf-8
+import random
+import threading
+import time
+
+
+def thread_run(urls):
+    for url in urls:
+        time.sleep(random.random())
+        print(url)
+
+
+t1 = threading.Thread(target=thread_run, args=([1, 2, 3],))
+t2 = threading.Thread(target=thread_run, args=([4, 5, 6],))
+t1.start()
+t2.start()
+
+
+class MyThreading(threading.Thread):
+
+    def __init__(self, name, urls):
+        super().__init__(name=name)
+        self.urls = urls
+
+    def run(self):
+        for url in self.urls:
+            time.sleep(random.random())
+            print(url)
+
+
+m1 = MyThreading(name='m1', urls=[11, 12, 13])
+m2 = MyThreading(name='m2', urls=[14, 15, 16])
+m1.start()
+m2.start()
+
+
+class MyThreading1(threading.Thread):
+    def __init__(self, name):
+        super().__init__(name=name)
+
+    def run(self):
+        global num
+        while True:
+            my_lock.acquire()
+            if num > 104:
+                my_lock.release()
+                break
+            num += 1
+            my_lock.release()
+            print(str(num))
+
+
+my_lock = threading.RLock()
+num = 100
+m3 = MyThreading1(name='m3')
+m4 = MyThreading1(name='m4')
+m3.start()
+m4.start()
 '''
 1. threading模块创建多线程
 
